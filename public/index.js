3146e4b1587c6b3c1f7e38dd381f8370437f1642
UfukE <emretus@gmail.com>
2020-05-20 17:07:15 +0300

    Developing...

---
+++ public/index.js
@@ -0,0 +1,169 @@
+
+const socket = io();
+socket.on("connect",()=>console.log("Successfully connected to the server."));
+socket.on("disconnect",()=>console.log("Connection failed with the server."));
+socket.on("OPPONENT_DISCONNECT",()=>console.log("dd2341d"));
+socket.on("JH",players=>{
+    globalIntervals.forEach(e=>clearInterval(e));
+    document.body.removeChild(document.getElementById("main"));
+    document.body.style.backgroundColor = "transparent";
+    canvas.style.visibility = "visible";
+    gameinit(players);
+});
+
+const globalIntervals = [];
+document.getElementById("create-lobby").onclick = function(e){
+    let clickfunc = this.onclick;
+    this.firstElementChild.innerText = socket.id;
+    document.getElementById("join-lobby").style.visibility = "hidden";
+
+    const waiting = document.createElement("a");
+    waiting.setAttribute("href","#");
+    waiting.setAttribute("class","detext loading");
+    waiting.style.cursor = "default";
+    waiting.innerText = "Waiting for player2 to join";
+    let h = document.getElementById("main").appendChild(document.createElement("h2").appendChild(waiting).parentNode);
+    globalIntervals.push(setInterval(()=>socket.emit("LOBBY_WAIT_PLAYER",socket.id),2000));
+
+
+    this.onclick = function(e){
+        this.firstElementChild.innerText = "Create Lobby";
+        document.getElementById("join-lobby").style.visibility = "visible";
+        document.getElementById("main").removeChild(h);
+        globalIntervals.forEach(el=>clearInterval(el));
+        this.onclick = clickfunc;
+    }
+}
+
+document.getElementById("join-lobby").onclick = function(e){
+    let clickfunc = this.onclick;
+    document.getElementById("create-lobby").style.visibility = "hidden";
+    this.style.visibility = "hidden";
+
+    const input = document.createElement("input");
+    input.setAttribute("placeholder","Enter lobby id")
+    input.setAttribute("class","inp");
+    document.getElementById("main").appendChild(input);
+
+
+    input.onkeydown = function(e){
+        if(e.key=="Enter"){
+        socket.emit("LOBBY_JOIN_PLAYER",this.value);
+        document.getElementById("create-lobby").style.visibility = "visible";
+        document.getElementById("join-lobby").style.visibility = "visible";
+        document.getElementById("main").removeChild(input);
+        document.getElementById("join-lobby").onclick = clickfunc;
+        }
+    }
+}
+let reverse = 1;
+const canvas = document.getElementById("screen");
+const context = canvas.getContext("2d");
+const fps = 30;
+let ball = {};
+
+class Paddle {
+    constructor(reverse,w,h,color,speed,{x,y}={
+        true: {x:30,y:canvas.height/2},
+        false:{x:canvas.width-30,y:canvas.height/2}
+    }[reverse]){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.color = color;
+        this.speed = speed;
+        this.reverse = reverse;
+    }
+
+    render(s){
+        s.save();
+        s.fillStyle = this.color;
+        s.fillRect(this.x-this.w/2,this.y-this.h/2,this.w,this.h);
+        s.restore();
+    }
+
+    move(dir){
+        this.y += dir*this.speed;
+    }
+
+}
+
+async function gameinit(players){
+    await setup(players);
+    draw(players);
+    //setInterval(draw,1000/fps,players)
+}
+
+socket.on("BALL",b => {
+    ball={
+        ...b,
+    render: function(s){
+        s.save();
+        s.fillStyle = "white";
+        s.fillRect(this.x-this.w/2,this.y-this.h/2,this.w,this.h);
+        s.restore();
+    },
+    /*update: function(){
+        this.x += this.velx;
+        this.y += this.vely;
+    },
+
+    collision: function(paddles){
+        if(this.x+this.w/2 > 600 || this.x-this.w/2 < 0) this.velx*=-1;
+        if(this.y+this.h/2 >= 400) this.vely=-Math.abs(this.vely);
+        if(this.y-this.h/2 <= 0) this.vely=Math.abs(this.vely);
+        if(paddles.some(paddle=>this.y+this.h/2 > paddle.y-paddle.h/2 && this.y-this.h/2 < paddle.y+paddle.h/2 && (Math.abs((this.x-this.w/2)-(paddle.x+paddle.w/2)) < 5 || Math.abs((this.x+this.w/2)-(paddle.x-paddle.w/2)) < 5 ))){
+            this.velx*=-1;
+            this.vely*=-1;
+        }
+    }*/
+    };
+});
+
+async function setup(players){
+    let info = players.INFO;
+    delete players.INFO;
+    if (players["player1"].id==info.selfID){
+        context.translate(canvas.width, 0);
+        context.scale(-1,1);
+    };
+    console.log(players);    
+    players["player1"].paddle = new Paddle(players["player1"].id==info.lobbyID,20,80,"red",2);
+    players["player2"].paddle = new Paddle(players["player1"].id==info.selfID,20,80,"blue",2);
+
+    onkeydown = e => {
+        if(e.key == "ArrowUp" || e.key == "keyW"){
+            players.player1.paddle.move(-10);
+            socket.emit("PLAYER_UPDATE",players);
+        }
+
+        if(e.key == "ArrowDown" || e.key == "keyS"){
+            players.player1.paddle.move(10);
+            socket.emit("PLAYER_UPDATE",players);
+
+        }
+
+    }
+
+    socket.on("PLAYER_RECIEVED",ps=>{
+        players.player2.paddle.y = ps.paddle.y;
+    })
+
+
+    return (void 0);
+}
+
+
+function draw(players){
+    context.save();
+    context.clearRect(0,0,canvas.width,canvas.height);
+    for (const player in players) {
+        players[player].paddle.render(context);
+    }
+    context.restore();
+    socket.emit("BALL_COLLISION",[...Object.values(players).map(e=>e.paddle)])
+    socket.emit("BALL_UPDATE",true);
+    ball.render(context);
+    setTimeout(requestAnimationFrame,1000/fps,()=>draw(players));
+}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,94 @@
 const express = require("express");
-const app = express()
-const port = process.env.PORT || 3000
+const http = require("http");
+const socketio = require("socket.io");
 
-app.use(express.static("/../public"));
-app.listen(port,()=>console.log("Server is running..."))
+const app = express()
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = socketio(server);
+const joinIDs = [];
+app.use(express.static("public"));
+server.listen(port,()=>console.log("Server is running..."))
+const pmap = (n, start1, stop1, start2, stop2)  => (n - start1) / (stop1 - start1) * (stop2 - start2) + start2;
+const Ball = function()
+{
+    this.x = 600/2;
+    this.y = 400/2;
+    this.w = 10;
+    this.h = 10;
+    this.speed = 3;
+    this.angle = 190*Math.PI/180//Math.random() * Math.PI/4 * (Math.random()>0.5 ? -1 : 1);
+    this.velx = this.speed * Math.cos(this.angle);
+    this.vely = this.speed * Math.sin(this.angle);
+    this.update = function(){
+        this.x += this.velx;
+        this.y += this.vely;
+    }
+    this.collision = function(paddles){
+        let pl = paddles.find(e=>e.x==30); 
+        let pr = paddles.find(e=>e!=pl);
+        //console.log("Pl:",pl,"\nPr: ",pr,"\nBall: ",this);
+        if(this.x+this.w/2 > pr.x+pr.w/2 || this.x-this.w/2 < pl.x-pl.w/2) {
+            return new Ball();
+        }
+        if(this.y+this.h/2 >= 400) this.vely=-Math.abs(this.vely);
+        if(this.y-this.h/2 <= 0) this.vely=Math.abs(this.vely);
+        let pad = paddles.find(paddle=>this.y+this.h/2 > paddle.y-paddle.h/2 && this.y-this.h/2 < paddle.y+paddle.h/2 && (Math.abs((this.x-this.w/2)-(paddle.x+paddle.w/2)) < 2 || Math.abs((this.x+this.w/2)-(paddle.x-paddle.w/2)) < 2 ));
+        if(pad){  
+            let ofs = pad.x==30 ? 45 : 135;
+            this.angle = pmap(this.y,pad.y-pad.h/2,pad.y+pad.h/2,-ofs,ofs)
+            this.velx = this.speed * Math.cos(this.angle/180*Math.PI) * (this.reverse ? -1 : 1);
+            this.vely = this.speed * Math.sin(this.angle/180*Math.PI);;
+        }
+        return this;
+    }
+};
+io.on("connection",socket=>{
+    console.log("User connected to the server");
+
+    socket.on("disconnect",s=>{
+        socket.emit("OPPONENT_DISCONNECT")
+        console.log("User disconnected from the server");
+        joinIDs.find(el=>el.selfID===s.id) ?  io.sockets.connected[joinIDs.find(el=>el.selfID===s.id).lobbyID].disconnect() : (joinIDs.find(el=>el.lobbyID===s.id) ? io.sockets.connected[joinIDs.find(el=>el.lobbyID===s.id).selfID].disconnect() : null);
+
+    })
+
+    socket.on("LOBBY_WAIT_PLAYER",s=>{
+        joinIDs.forEach(e=>{
+            if(e.lobbyID===s){
+                io.to(s).emit("JH",{player1:{id:s},player2:{id:e.selfID},INFO:e});
+                io.to(e.selfID).emit("JH",{player1:{id:e.selfID},player2:{id:s},INFO:e});
+            }
+        });
+    });
+
+    socket.on("LOBBY_JOIN_PLAYER",id => {
+        joinIDs.push({selfID:socket.id,lobbyID:id,ball:new Ball()});
+        joinIDs.forEach(e=>{
+            io.sockets.connected[e.selfID] && io.sockets.connected[e.selfID].emit("BALL",e.ball);
+            io.sockets.connected[e.lobbyID] && io.sockets.connected[e.lobbyID].emit("BALL",e.ball);
+        });
+    });
+
+    socket.on("PLAYER_UPDATE",players => {
+        io.sockets.connected[players.player2.id] && io.sockets.connected[players.player2.id].emit("PLAYER_RECIEVED",players.player1);
+    });
+
+    socket.on("BALL_UPDATE",()=>{
+        joinIDs.forEach(e=>{
+            e.ball.update();
+            io.sockets.connected[e.selfID] && io.sockets.connected[e.selfID].emit("BALL",e.ball);
+            io.sockets.connected[e.lobbyID] && io.sockets.connected[e.lobbyID].emit("BALL",e.ball);
+        });
+    })
+
+    socket.on("BALL_COLLISION",ar=>{
+        joinIDs.forEach(e=>{
+            e.ball = e.ball.collision(ar);
+            io.sockets.connected[e.selfID] && io.sockets.connected[e.selfID].emit("BALL",e.ball);
+            io.sockets.connected[e.lobbyID] && io.sockets.connected[e.lobbyID].emit("BALL",e.ball);
+        });
+    })
+
+});
+
